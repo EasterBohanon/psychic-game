09b799b0693ca9021c1ad592b0cc1941608af6b3
Easter Bohanon <eastdabeast@email.arizona.edu>
2018-11-17 01:21:28 -0700

    Now I just need it to work

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -14,10 +14,74 @@
 
  // Let's the user have 10 total guesses
 
- function revisedGuessesRemaining() {
+ function reviseGuessesRemaining() {
      document.querySelector('#guesses-left').innerHTML= "Guesses left:" + guessesRemaining;
+ };
+// Will update the amount of letters left to guess
+ function reviseLetterToGuess() {
+     this.lettersToGuess = this.computerChoices[Math.floor(Math.random() * this.computerChoices.length)];
+ };
+// Will display letters that have been entered by the user
+ function reviseGuessesMade() {
+     document.querySelector('#guesses-so-far').innerHTML = "Your Guesses so far:" + guessedLetters.join(', ');
+
+ };
+
+ //Will be called and reset everything once the first game is done
+ var reset = function() {
+     totalGuesses = 10;
+     guessesRemaining = 10;
+     guessedLetters = [];
+
+     reviseLetterToGuess();
+     reviseGuessesRemaining();
+     reviseGuessesMade();
  }
 
- function GuessesSoFar() {
-     
- }
+ reviseLetterToGuess();
+reviseGuessesRemaining();
+
+// When the key is released it will be the User's guess
+
+document.onekeyup = function(event) {
+    var userGuess = string.fromCharCode(event.keycode).toLowerCase();
+    var check = computerChoices.includes(userGuess);
+
+    // will appear when error occurs
+    if (check === false) {
+        alert("Hope you're not celebrating, because you WRONG, Try again!");
+        return false;
+
+    }
+
+    else if (check === true) {
+        guessesRemaining--;
+        guessedLetters.push(userGuess);
+        reviseGuessesRemaining();
+        reviseGuessesMade();
+
+        if (guessesRemaining > 0) {
+            if (userGuess == lettersToGuess) {
+                wins++;
+                document.querySelector('#wins').innerHTML = "Wins:" + wins;
+                userGuess = userGuess.toUpperCase();
+                alert("Well, it seems as if you maybe the chosen one. Can you do it again?");
+                reset();
+    
+            }
+        }
+        else if (guesses == 0) {
+            //When someone loses completely this will let them know and reset
+            losses++;
+            document.querySelector('#Losses').innerHTML = "Losses:" + losses;
+            alert("You didn't see this coming, but, you failed the Psychic test, you could try again ");
+            reset();
+
+        }
+        return false;
+    }
+    else {
+        alert ("something is wrong, we have an error!");
+    }
+};
+
